5b5bc0d79e097f9c0e02b3467b79cfee52e67983
tcotidiane33 <tcotidiane33@gmail.com>
2024-01-02 18:26:24 +0000

    create DB on MONGODB

--- src/components/Pages/FormPostArt.js
+++ src/components/Pages/FormPostArt.js
@@ -14,23 +14,54 @@ export default function FormPostArt() {
                     <br />
                     <MDBTextArea label='Description' id='textAreaExample' rows={4} />
                     <br />
-                    <MDBInput label='marque' id='formControlDefault' type='text' />
-
+                    <MDBInput label='Type' id='formControlDefault' type='text' />
+                    <div className="col-lg-12 mt-5">
+                        <label htmlFor="selectOptions">Type :</label>
+                        <select id="selectOptions" className="form-select" onChange={(e) => console.log("Selected value:", e.target.value)}>
+                            <option value="Type">Option de type</option>
+                            <option value="value1">Téléphones Mobiles</option>
+                            <option value="value2" >Ordinateurs</option>
+                            <option value="value3">Appareils Électroménagers</option>
+                            <option value="value4">Appareils Hi-Fi et Audio</option>
+                            <option value="value5">Télévisions</option>
+                            <option value="value6">Divers</option>
+                        </select>
+                    </div>
+                    <br />
                     <div className="col-lg-12 mt-5">
                         <label htmlFor="selectOptions">Marques:</label>
                         <select id="selectOptions" className="form-select" onChange={(e) => console.log("Selected value:", e.target.value)}>
-                            <option value="">Select an option</option>
-                            <option value="value1">Value 1</option>
-                            <option value="value2" selected>Value 2</option>
-                            <option value="value3">Value 3</option>
-                            <option value="value4">Value 4</option>
-                            <option value="value5">Value 5</option>
-                            <option value="value6">Value 6</option>
+                            <option value="Marque">Option de Marque</option>
+                            <option value="value1">Samsung</option>
+                            <option value="value2" >Apple (iPhone)</option>
+                            <option value="value3">Huawei</option>
+                            <option value="value4">Tecno Mobile</option>
+                            <option value="value5">Infinix</option>
+                            <option value="value6">Nokia</option>
+                            <option value="value6">HP (Hewlett-Packard)</option>
+                            <option value="value6">Dell</option>
+                            <option value="value6">Lenovo</option>
+                            <option value="value6">ASUS</option>
+                            <option value="value6">Acer</option>
+                            <option value="value6">LG</option>
+                            <option value="value6">Sony</option>
+                            <option value="value6">Panasonic</option>
+                            <option value="value6">Hisense</option>
+                            <option value="value6">TCL</option>
+                            <option value="value6">Bosch</option>
+                            <option value="value6">hirlpool</option>
+                            <option value="value6">Miele</option>
+                            <option value="value6">Bose</option>
+                            <option value="value6">Panasonic</option>
+                            <option value="value6">JBL</option>                            <option value="value6"></option>
+                            <option value="value6">Sennheiser</option>
+                            <option value="value6"></option>
+
                         </select>
                     </div>
 
                     <br />
-                    <MDBFile label='Default file input example' id='customFile' />
+                    <MDBFile label='pièces Jointes' id='customFile' />
                     <br />
                     <label>Ville:</label>
                     <br />
